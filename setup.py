d06dda978799f6732d9b070a538470f587d80818
Aradhya <aradhyatripathi51@gmail.com>
2022-11-17 21:04:29 +0530

    chore: Added requirements and manifest

--- setup.py
+++ setup.py
@@ -4,5 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="googleclient",
     author_email="aradhyatripathi51@gmail.com",
+    version="0.1.0",
     packages=find_packages(),
+    license="MIT",
 )
